cd485337292b7b1e6e12be1007b5c3188425a923
Brant Shapka <bshapka@gmail.com>
2017-08-31 23:45:02 -0700

    implemented savings rate bisection search, test cases all pass

--- ps1c.py
+++ ps1c.py
@@ -1,15 +1,12 @@
 # data supplied by the user
-annual_salary = float(input('Enter your annual salary: '))
-portion_saved = float(input('Enter the percent of your salary to save, as a decimal: '))
+base_annual_salary = float(input('Enter your annual salary: '))
 
 # data that is fixed
 portion_down_payment = 0.25
 rate_of_return = 0.04
 monthly_rate_of_return = rate_of_return / 12
-monthly_salary = annual_salary / 12
 total_cost = 1000000
 down_payment = total_cost * portion_down_payment
-monthly_deposit = monthly_salary * portion_saved
 semi_annual_raise = 0.07
 months = 36
 
@@ -20,18 +17,45 @@ current_savings = 0.0
 # there is an acceptable margin of error for this algorithm
 epsilon = 100
 
-# use exhaustive enumeration to find the solution
+# define high and low bounds for the bisection search
+initial_high = 10000
+high = initial_high
+low = 0
+portion_saved = (high + low) // 2
+steps = 0
+
+
+# use bisection search to find the solution
+search_space_empty = False
 while abs(current_savings - down_payment) > epsilon:
-    # problem states investment income and savings deposits occur at the end
-    # of the month, so increment month before mutating current_savings
-    months += 1
-    current_savings *= 1 + monthly_rate_of_return
-    current_savings += monthly_deposit
-    # problem states that semi-annual raises take effect the next month, so 
-    # mutate monthly_salary after mutating current_savings
-    if months % 6 == 0:
-        annual_salary *= 1 + semi_annual_raise
-        monthly_salary = annual_salary / 12
-        monthly_deposit = monthly_salary * portion_saved
+    current_savings = 0.0
+    annual_salary = base_annual_salary
+    monthly_salary = annual_salary / 12
+    monthly_deposit = monthly_salary * (portion_saved / 10000)
+    for month in range(1, months + 1):
+        current_savings *= 1 + monthly_rate_of_return
+        current_savings += monthly_deposit
+        # problem states that semi-annual raises take effect the next month, so 
+        # mutate monthly_salary after mutating current_savings
+        if month % 6 == 0:
+            annual_salary *= 1 + semi_annual_raise
+            monthly_salary = annual_salary / 12
+            monthly_deposit = monthly_salary * (portion_saved / 10000)
+    prev_portion_saved = portion_saved
+    if current_savings > down_payment:
+        high = portion_saved
+    else:
+        low = portion_saved
+    portion_saved = int(round((high + low) / 2))
+    # if portion_saved is no longer changing, our search space is no longer
+    # changing (because the search value is outside the search space), so we
+    # break to stop an infinite loop
+    if prev_portion_saved == portion_saved:
+        break
+    steps += 1
     
-print('Number of months:', months)
+if prev_portion_saved == portion_saved and portion_saved == initial_high:
+    print('It is not possible to pay the down payment in three years.')
+else:
+    print('Best savings rate:', portion_saved / 10000)
+    print('Steps in bisection search:', steps)
