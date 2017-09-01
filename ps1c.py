9fd2568402b42342723f49ad70359edb8af428f7
Brant Shapka <bshapka@gmail.com>
2017-08-31 21:30:48 -0700

    set variables whose values were formerly acquired from users to fixed values

--- ps1c.py
+++ ps1c.py
@@ -1,16 +1,16 @@
 # data supplied by the user
 annual_salary = float(input('Enter your annual salary: '))
 portion_saved = float(input('Enter the percent of your salary to save, as a decimal: '))
-total_cost = float(input('Enter the cost of your dream home: '))
-semi_annual_raise = float(input('Enter the semi-annual raise, as a decimal: '))
 
 # data that is fixed
 portion_down_payment = 0.25
 rate_of_return = 0.04
 monthly_rate_of_return = rate_of_return / 12
 monthly_salary = annual_salary / 12
+total_cost = 1000000
 down_payment = total_cost * portion_down_payment
 monthly_deposit = monthly_salary * portion_saved
+semi_annual_raise = 0.07
 
 # initially savings are zero. This variable is the core part of the decrementing
 # function used to stop the algorithm
