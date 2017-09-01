a95961445df7540244b92b870ee7e0c493c5caa1
Brant Shapka <bshapka@gmail.com>
2017-08-31 21:00:49 -0700

    implemented exaustive enumeration algorithm to find the solution, test cases passed

--- ps1a.py
+++ ps1a.py
@@ -9,6 +9,7 @@ rate_of_return = 0.04
 monthly_rate_of_return = rate_of_return / 12
 monthly_salary = annual_salary / 12
 down_payment = total_cost * portion_down_payment
+monthly_deposit = monthly_salary * portion_saved
 
 # initially savings are zero. This variable is the core part of the decrementing
 # function used to stop the algorithm
@@ -16,3 +17,13 @@ current_savings = 0.0
 
 # this variable will hold the solution we seek after completion of the algorithm
 months = 0
+
+# use exhaustive enumeration to find the solution
+while current_savings < down_payment:
+    # problem states investment income and savings deposits occur at the end
+    # of the month, so increment month before mutating current_savings
+    months += 1
+    current_savings *= 1 + monthly_rate_of_return
+    current_savings += monthly_deposit
+    
+print(months)
